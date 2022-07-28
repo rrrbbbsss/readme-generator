702da4daf534fe73af7f4893a6350c1c540a19b0
Royce Strange <roycest@swbell.net>
2022-07-28 17:04:31 -0500

    import modules

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fsPromises = require('fs/promises');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
